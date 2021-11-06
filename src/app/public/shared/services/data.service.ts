1b6d00b0c46d8d4ae1ac9f5fee8a46ba9d1b330b
Lanceopries <35199929+Lanceopries@users.noreply.github.com>
2021-11-06 12:00:37 +0300

    fix(data): data service improved

--- src/app/public/shared/services/data.service.ts
+++ src/app/public/shared/services/data.service.ts
@@ -411,9 +411,9 @@ export class SGRDataService {
     {
       title: 'Технопарк "Центр хайтек инноваций "РИКОР"',
     },
-    {
-      title: 'NoneTechParksType',
-    },
+    // {
+    //   title: 'NoneTechParksType',
+    // },
     {
       title: 'Технопарк "ИТЭЛМА"',
     },
